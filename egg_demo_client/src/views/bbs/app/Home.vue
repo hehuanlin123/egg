155afbdef7f15ec71cc16cacd1653a97f20fe38c
szkfzx <szkfzx@szkfzxdeiMac.local>
2020-04-17 15:50:10 +0800

    home

--- egg_demo_client/src/views/bbs/app/Home.vue
+++ egg_demo_client/src/views/bbs/app/Home.vue
@@ -62,11 +62,6 @@
         text-align: center;
     }
 
-    .layout footer {
-        position: absolute;
-        bottom: 0;
-    }
-
     .layout main {
         width: 100%;
         height: 100%;
